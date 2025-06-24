cb865f67387f1f23ded5d32ed872d4f2a197920e
Vitaly <coolhosta01@gmail.com>
2025-06-25 00:09:14 +0300

    feat: upd favicon

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,15 @@
 </template>
 
 <script setup lang="ts">
+import { useFavicon } from '@vueuse/core'
+
 import UseTheme from '@/composables/UseTheme'
 
 UseTheme()
+
+const icon = useFavicon()
+
+icon.value = 'favicon.svg'
 </script>
 
 <style lang="scss">
